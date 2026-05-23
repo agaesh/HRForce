36a8dfb4af6d9f30ebde36b8d2b9530c7b84e5c5
agaes <agaes@AGAESH>
2026-05-23 10:36:50 +0800

    feat: add department repository interface and connect to service

---
+++ HRForce.ApiService/Application/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,17 @@
+﻿using HRForce.ApiService.Domain;
+
+namespace HRForce.ApiService.Application.Interfaces
+{
+    public interface IDepartmentRepository
+    {
+        Task<List<Department>> GetAllDepartmentsAsync();
+
+        Task<Department?> GetDepartmentByIdAsync(int id);
+
+        Task<Department> CreateAsync(Department department);
+
+        Task UpdateAsync(Department department);
+
+        Task DeleteAsync(Department department);
+    }
+}

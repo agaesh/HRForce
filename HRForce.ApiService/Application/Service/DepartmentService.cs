e983bcebabf194f2e17358cd1433b02f09806a8d
agaes <agaes@AGAESH>
2026-05-23 10:48:20 +0800

    feat - Initialize DepartmentService with DepartmentRepository variable

---
+++ HRForce.ApiService/Application/Service/DepartmentService.cs
@@ -0,0 +1,12 @@
+﻿using HRForce.ApiService.Application.Interfaces;
+using HRForce.ApiService.Infrastructure.Repositories;
+namespace HRForce.ApiService.Application.Service
+{
+    public class DepartmentService
+    {
+        public readonly DepartmentRepository _departmentRepository;
+        public DepartmentService(IDepartmentRepository departmentRepository) { 
+           _departmentRepository = (DepartmentRepository)departmentRepository;
+        }
+    }
+}

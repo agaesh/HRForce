246533d9acc45f1ec1bd5b7048ee2d5d4a284dc9
agaes <agaes@AGAESH>
2026-05-24 19:01:49 +0800

    refactor - Improve claritiy in employee repository method name
    - updated the Interface method signature

--- HRForce.ApiService/Application/Interfaces/IEmployeeRepository.cs
+++ HRForce.ApiService/Application/Interfaces/IEmployeeRepository.cs
@@ -4,8 +4,8 @@ namespace HRForce.ApiService.Application.Interfaces
 {
     public interface IEmployeeRepository
     {
-        Task<List<Employee>> GetAllAsync();
-        Task<Employee?> GetByIdAsync(int id);
+        Task<List<Employee>> GetAllEmployeesAsync();
+        Task<Employee?> GetEmployeeByIdAsync(int id);
         Task<Employee> CreateAsync(Employee employee);
         Task UpdateAsync(Employee employee);
         Task DeleteAsync(int id);

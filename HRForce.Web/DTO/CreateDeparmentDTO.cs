2955e3989f03db597907b56212e4055905f65a7d
agaes <agaes@AGAESH>
2026-05-25 17:04:52 +0800

    feat - add Client Side DTO for sending request
    - CreateDepartmentDTO - To Send Create Request
    - DepartmentDTO - to Get Listing
    -UpdateDepartmentDTO - For sending update request

---
+++ HRForce.Web/DTO/CreateDeparmentDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace HRForce.Web.DTO
+{
+    public class CreateDepartmentDto
+    {
+        public required string DepartmentCode { get; set; } // e.g., "HR", "IT", "FIN"
+        public required string DepartmentName { get; set; }
+        public required string Status { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ HRForce.Web/DTO/UpdateDepartmentDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRForce.Web.DTO
+{
+    public class UpdateDepartmentDTO
+    {
+        public int Id { get; set; }
+        public string DepartmentName { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}

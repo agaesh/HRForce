96d5fef2f5430e265a7d1815ba1ccf73b0a241ac
agaes <agaes@AGAESH>
2026-05-28 21:41:06 +0800

    feat- add client level dtos
    -APIResponseDTO to Deserialzie api response and print errors in the the blazor template
    - CreateDepartmentDTO used to send POST Request from client to server. it binds value from the form.
    - Update Deparment DTO used to update existing values , bind it to the DTO to update

--- HRForce.Web/DTO/CreateDeparmentDTO.cs
+++ HRForce.Web/DTO/CreateDeparmentDTO.cs
@@ -3,9 +3,9 @@ namespace HRForce.Web.DTO
 {
     public class CreateDepartmentDto
     {
-        public required string DepartmentCode { get; set; } // e.g., "HR", "IT", "FIN"
-        public required string DepartmentName { get; set; }
-        public required string Status { get; set; }
+        public string DepartmentCode { get; set; } // e.g., "HR", "IT", "FIN"
+        public string DepartmentName { get; set; }
+        public string Status { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
--- HRForce.Web/DTO/UpdateDepartmentDTO.cs
+++ HRForce.Web/DTO/UpdateDepartmentDTO.cs
@@ -4,7 +4,6 @@ namespace HRForce.Web.DTO
 {
     public class UpdateDepartmentDTO
     {
-        public int Id { get; set; }
         public string DepartmentName { get; set; }
 
         public string Status { get; set; }

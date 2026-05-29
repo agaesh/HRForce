411a51cc253c9a73781207506e2f588898c1b897
agaes <agaes@AGAESH>
2026-05-29 11:23:50 +0800

    feat(department-service): add paged query with search

--- HRForce.ApiService/Application/Interfaces/IDepartmentService.cs
+++ HRForce.ApiService/Application/Interfaces/IDepartmentService.cs
@@ -1,11 +1,13 @@
-﻿using HRForce.ApiService.Domain;
-using HRForce.ApiService.Application.DTO;
+﻿using HRForce.ApiService.Application.DTO;
+using HRForce.ApiService.Domain;
+using HRForce.ApiService.Helpers;
 using Microsoft.AspNetCore.Mvc;
 namespace HRForce.ApiService.Application.Interfaces
 {
     public interface IDepartmentService
     {
-        Task<List<DepartmentDTO>> GetAllDepartmentsAsync();
+        Task<PagedResult<DepartmentDTO>> GetAllDepartmentsQueryable(int pageNumber = 1, int pageSize = 10, string search = null);
+
 
         Task<DepartmentDTO?> GetDepartmentByIdAsync(int id);
 
